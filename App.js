c3c6300cfb212823e3350f40701a48486f3c2c03
Jeong <qsz78547@naver.com>
2021-10-12 21:36:01 +0900

    리액트 연습 1

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject  from './components/Subject';
+
+function App() {
+  return (
+    <div className="App">
+      <Subject title ="WEB" sub="world wide WEB!"></Subject>
+      <TOC></TOC> 
+      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Subject.js
@@ -0,0 +1,9 @@
+function Subject(props){
+    return (
+      <header>
+        <h1>{props.title}</h1>
+        {props.sub}
+      </header>
+    );
+  }
+  export default Subject;
